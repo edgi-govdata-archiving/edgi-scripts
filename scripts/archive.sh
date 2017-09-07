5f10bbfac00ee45c03e7ccecdf6a50290cfd680c
Patrick Connolly <patrick.c.connolly@gmail.com>
2017-09-07 05:08:57 -0400

    Used mktemp to get safe temp path. [#11] h/t @mhucka

--- scripts/archive.sh
+++ scripts/archive.sh
@@ -6,15 +6,19 @@ URL=$1
 PATH=$2
 EXCLUDE=$3
 
-/bin/rm -rf "$URL"
+TMP_PATH=$(/bin/mktemp --directory)
 
 echo "Gathering URLs..."
 
-/usr/bin/wget -q -r -nc "$URL$PATH" -D "$URL" -X "$EXCLUDE" -A html
+/usr/bin/wget --quiet --recursive --no-clobber "$URL$PATH" \
+  --domains "$URL" \
+  --directory-prefix "$TMP_PATH" \
+  --exclude-directories "$EXCLUDE" \
+  --accept html
 
 echo "Sending to archive.org..."
 
-/usr/bin/find "$URL" -type f -print0 | while IFS= read -r -d $'\0' LINE; do
+/usr/bin/find "$TMP_PATH" -type f -print0 | while IFS= read -r -d $'\0' LINE; do
     LINE=$(echo $LINE | /bin/sed -e 's/\(index.html\)*$//g')
     echo "Saving $LINE"
     /usr/bin/curl -s "https://web.archive.org/save/$LINE" > /dev/null &
@@ -23,4 +27,4 @@ done
 
 echo "Done"
 
-/bin/rm -rf "$URL"
+/bin/rm -rf "$TMP_PATH"
